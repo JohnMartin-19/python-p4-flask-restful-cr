c2f789762956f0d31c463a9980963a50bdbd560f
John <johnnkonge2020@gmail.com>
2024-01-10 09:27:59 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -7,7 +8,7 @@ from models import db, Newsletter
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -24,9 +25,62 @@ class Index(Resource):
 
         response = make_response(
             jsonify(response_dict),
-            200
+            200,
         )
 
         return response
 
 api.add_resource(Index, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
+        return response
+
+    def post(self):
+
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
