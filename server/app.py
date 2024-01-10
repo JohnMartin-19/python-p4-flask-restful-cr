c78aa0644270cc7791ca1b8c9690ea4ad4c8bf42
John <johnnkonge2020@gmail.com>
2024-01-10 09:13:27 +0300

    coded along

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -9,15 +7,26 @@ from models import db, Newsletter
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-    pass
+class Index(Resource):
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    def get(self):
+
+        response_dict = {
+            "index": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+api.add_resource(Index, '/')
